41df6d9334c4d8127b416d79136845425a6aa606
77web <info@77-web.com>
2015-04-11 14:50:00 +0900

    Elementを作るElementFactory作成

---
+++ src/ElementFactory.php
@@ -0,0 +1,20 @@
+<?php
+
+
+namespace Nagoya\Mondai9;
+
+
+use Nagoya\Mondai9\Entity\Element;
+
+class ElementFactory
+{
+    public function create(array $config)
+    {
+        $element = new Element();
+        $element->id = $config['id'];
+        $element->caption = $config['value'];
+        $element->parentId = $config['parent'];
+
+        return $element;
+    }
+}
